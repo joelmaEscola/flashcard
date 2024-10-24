dd95388dac3a3b4614268c0624972ea103840ba9
joelmaEscola <140757363+joelmaEscola@users.noreply.github.com>
2024-10-24 07:42:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,17 @@
 criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quem foi Stephen Hawking?',
+    'Foi um físico teórico, cosmólogo e autor britânico, reconhecido internacionalmente por sua contribuição à ciência, sendo um dos mais renomados cientistas do século'
 )
 
 criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quem foi Albert Einstein?',
+    'Foi um físico teórico alemão, que desenvolveu a teoria da relatividade geral, um dos pilares da física moderna ao lado da mecânica quântica.'
 )
 
 criaCartao(
     'categoria',
-    'pergunta',
-    'resposta'
+    'Quem foi Nikola Tesla?',
+    'Foi um inventor, engenheiro eletrotécnico e engenheiro mecânico sérvio, mais conhecido por suas contribuições ao projeto do moderno sistema de fornecimento de eletricidade em corrente alternada.'
 )
